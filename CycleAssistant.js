9704d20196d1a82e27498f66a7385eed216c8fcc
BillMills <mills.wj@gmail.com>
2014-03-07 16:13:42 -0800

    buggy implementation of relaistic activity profile

--- CycleAssistant.js
+++ CycleAssistant.js
@@ -28,12 +28,14 @@ function tableScrape(){
 function generateDataCSV(state){
 	var data = (state == 'cycle') ? 'Time[s]' : 'Time[h]',
 		nPoints = 800,
-		key, i, nextline;
+		key, i, nextline,
+		lastActivity = {};
 
 	//CSV header row:
 	for(key in window.isotopeList){
 		data += ',';
 		data += key;
+		lastActivity[key] = 0; //start activities at 0
 	}
 	if(data == 'Time[h]' || data == 'Time[s]')
 		data += ','; //blank column for page load
@@ -52,11 +54,15 @@ function generateDataCSV(state){
 		for(key in window.isotopeList){
 			if(window.isotopeList[key].visible){
 				nextline += ',';
-				if(state == 'during')
-					nextline += activityDuring(window.isotopeList[key].yield, window.isotopeList[key].lifetime, (window.cycleParameters.duration / nPoints)*i*3600);
-				else if(state == 'cycle')
-					nextline += activityDuring(window.isotopeList[key].yield, window.isotopeList[key].lifetime, (3*(window.cycleParameters.beamOn + window.cycleParameters.beamOff) / nPoints)*i/1000);
-				else if(state == 'after')
+				if(state == 'during'){
+					lastActivity[key] = activity(Math.max(0, (window.cycleParameters.duration / nPoints)*(i-1)*3600000), lastActivity[key], window.isotopeList[key].yield, window.isotopeList[key].lifetime, (window.cycleParameters.duration / nPoints)*i*3600000 )
+					//nextline += activityDuring(window.isotopeList[key].yield, window.isotopeList[key].lifetime, (window.cycleParameters.duration / nPoints)*i*3600);
+					nextline += lastActivity[key];
+				} else if(state == 'cycle'){
+					lastActivity[key] = activity(Math.max(0, (3*(window.cycleParameters.beamOn + window.cycleParameters.beamOff) / nPoints)*(i-1) ), lastActivity[key], window.isotopeList[key].yield, window.isotopeList[key].lifetime, (3*(window.cycleParameters.beamOn + window.cycleParameters.beamOff) / nPoints)*i )
+					//nextline += activityDuring(window.isotopeList[key].yield, window.isotopeList[key].lifetime, (3*(window.cycleParameters.beamOn + window.cycleParameters.beamOff) / nPoints)*i/1000);
+					nextline += lastActivity[key];
+				} else if(state == 'after')
 					nextline += activityAfter(window.isotopeList[key].yield, window.isotopeList[key].lifetime, (1000 / nPoints)*i*3600);
 			}
 		}
@@ -138,6 +144,95 @@ function activityAfter(yield, lifetime, time){
 	return peakActivity * Math.exp(-1*lifetime*time);
 }
 
+//starting with initial activity <A0>, return Activity after <time>, under beam <rate> for isotope with <lifetime>
+function stepActivity(A0, rate, lifetime, time){
+	return rate * (1 - Math.exp(-1*lifetime*time)) + A0 * Math.exp(-1*lifetime*time);
+}
+
+//calculate the activity at time <t>, given a beam-on <rate>, isotope <lifetime>, and that the activity was <A0> at time <t0>, 
+//t and t0 in ms
+function activity(t0, A0, rate, lifetime, t){
+	var beamOn, firstPeriodTimeElapsed,
+		propTime,
+		stepTime = t0,
+		stepA = A0,
+		scaleConstant;
+
+	//scale for each region:
+	if(window.region == 'tape') scaleConstant = 1//0.89;
+	else if(window.region == 'chamber') scaleConstant = 1//0.01;
+	else if(window.region == 'beamline') scaleConstant = 1//0.1;
+
+	//figure out if beam is on or off at time t0, and how long until the next cycle:
+	firstPeriodTimeElapsed = t0 % (window.cycleParameters.beamOn + window.cycleParameters.beamOff);
+	if(firstPeriodTimeElapsed < window.cycleParameters.beamOn) beamOn = true; //ie beam is on and has been on for <firstPeriodTimeElapsed>
+	else{
+		firstPeriodTimeElapsed -= window.cycleParameters.beamOn;  //beam has been off for this long
+		beamOn = false; //beam is off
+	}
+
+	//finish the state the beam was in at t0, if there's enough time before t.
+	if(beamOn) //beam is on
+		propTime = window.cycleParameters.beamOn - firstPeriodTimeElapsed;
+	else //beam is off
+		propTime = window.cycleParameters.beamOff - firstPeriodTimeElapsed;
+	//cut off propagation at time t if necessary:
+	propTime = Math.min(propTime, t-t0);
+	//propagate to the end of this state:
+	if(beamOn)
+		stepA = stepActivity(stepA, rate, lifetime, propTime/1000); //lifetimes recorded in seconds
+	else
+		stepA = stepActivity(stepA, 0, lifetime, propTime/1000);
+	stepTime += propTime;
+
+	//step through whole cycles until we get as close to t without going over 
+	while(stepTime + window.cycleParameters.beamOn + window.cycleParameters.beamOff < t){
+		//switch the beam state
+		beamOn = !beamOn;
+
+		if(beamOn){
+			propTime = window.cycleParameters.beamOn;
+			stepA = stepActivity(stepA, rate, lifetime, propTime/1000);
+		}
+		else{
+			propTime = window.cycleParameters.beamOff;
+			stepA = stepActivity(stepA, 0, lifetime, propTime/1000);
+		}
+		stepTime += propTime;
+	}
+
+	//we're now less than one cycle away from t.  Attempt to propagate one step:
+	beamOn = !beamOn;
+	if(beamOn) //beam is on
+		propTime = window.cycleParameters.beamOn;
+	else //beam is off
+		propTime = window.cycleParameters.beamOff;
+	//cut off propagation at time t if necessary:
+	propTime = Math.min(propTime, t-stepTime);
+	//propagate to the end of this state:
+	if(beamOn)
+		stepA = stepActivity(stepA, rate, lifetime, propTime/1000);
+	else
+		stepA = stepActivity(stepA, 0, lifetime, propTime/1000);
+	stepTime += propTime;	
+
+	//return if we've reached t:
+	if(stepTime == t)
+		return scaleConstant*stepA;
+
+	//finish the last step if we didn't just return:
+	beamOn = !beamOn;
+	propTime = t-stepTime;
+	//propagate to the end of this state:
+	if(beamOn)
+		stepA = stepActivity(stepA, rate, lifetime, propTime/1000);
+	else
+		stepA = stepActivity(stepA, 0, lifetime, propTime/1000);
+
+	return scaleConstant*stepA;
+
+}
+
 //repaint dygraphs as necessary:
 function repaint(){
 	var onDisplay = document.getElementById('stateFlag').state;
@@ -166,10 +261,8 @@ function cycleParameterScrape(){
 		'duration' : duration * durationUnit,
 		'dutyCycle' : onTime / (onTime + offTime)
 	}
-	
-	repaint();
 
-	console.log(window.cycleParameters);
+	repaint();
 }
 
 //return the value of a selected option from a <select> element
